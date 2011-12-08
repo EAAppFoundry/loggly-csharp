8c515722f10009a05586d94981e403d2d8a97a38
don browning <don.browning@gmail.com>
2011-12-08 12:48:57 -0500

    added the structured logger class, and added overloads to Log() and LogAsync() to better handle logging in a structured form

--- loggly-csharp/Logger.cs
+++ loggly-csharp/Logger.cs
@@ -7,49 +7,65 @@ namespace Loggly
 {
    public class Logger : ILogger, IRequestContext
    {
-      private const string _url = "logs.loggly.com/";
-      private readonly string _inputKey;
+        private const string _url = "logs.loggly.com/";
+        private readonly string _inputKey;
 
 
-      public Logger(string inputKey)
-      {
-         _inputKey = inputKey;
-      }
+        public Logger(string inputKey)
+        {
+            _inputKey = inputKey;
+        }
 
-      public LogResponse Log(string message)
-      {
-         var synchronizer = new AutoResetEvent(false);
-         LogResponse response = null;
-         LogAsync(message, r =>
-         {
+        public LogResponse Log(string message)
+        {
+            var synchronizer = new AutoResetEvent(false);
+            LogResponse response = null;
+            LogAsync(message, r =>
+            {
             response = r;
             synchronizer.Set();
-         });
-         synchronizer.WaitOne();
-         return response;
-      }
+            });
+            synchronizer.WaitOne();
+            return response;
+        }
 
-      public void LogAsync(string message)
-      {
-         LogAsync(message, null);
-      }
+        public LogResponse Log(StructuredLogLevel level, string machineName, string message)
+        {
+            string lev = Enum.GetName(typeof(StructuredLogLevel), level);
 
-      public void LogAsync(string message, Action<LogResponse> callback)
-      {
-         var communicator = new Communicator(this);
-         var callbackWrapper = callback == null ? (Action<Response>) null : r =>
-         {
+            StructuredLog l = new StructuredLog { Level = lev, MachineName = machineName, Message = message };
+
+            return Log(l.ToJSON());
+        }
+
+        public void LogAsync(StructuredLogLevel level, string machineName, string message)
+        {
+            string lev = Enum.GetName(typeof(StructuredLogLevel), level);
+            StructuredLog l = new StructuredLog { Level = lev, MachineName = machineName, Message = message };
+            LogAsync(l.ToJSON());
+        }
+
+        public void LogAsync(string message)
+        {
+            LogAsync(message, null);
+        }
+
+        public void LogAsync(string message, Action<LogResponse> callback)
+        {
+            var communicator = new Communicator(this);
+            var callbackWrapper = callback == null ? (Action<Response>) null : r =>
+            {
             if (r.Success)
             {
-               callback(JsonConvert.DeserializeObject<LogResponse>(r.Raw));
+                callback(JsonConvert.DeserializeObject<LogResponse>(r.Raw));
             }
-         };
-         communicator.SendPayload(Communicator.POST, string.Concat("inputs/", _inputKey), message, callbackWrapper);
-      }
+            };
+            communicator.SendPayload(Communicator.POST, string.Concat("inputs/", _inputKey), message, callbackWrapper);
+        }
 
-      public string Url
-      {
-         get { return _url; }
-      }
+        public string Url
+        {
+            get { return _url; }
+        }
    }
 }
---
+++ loggly-csharp/StructuredLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Loggly
+{
+    public enum StructuredLogLevel
+    {
+        info,
+        debug,
+        warn,
+        fatal,
+        error
+    }
+
+    public class StructuredLog
+    {
+        [JsonProperty("level")]
+        public string Level { get; set; }
+        [JsonProperty("machine")]
+        public string MachineName { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return "[" + Level.ToString() + "] - Machine: " + MachineName + " - " + Message;
+        }
+
+        public string ToJSON()
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(this);
+            return json;
+        }
+
+    }
+}
